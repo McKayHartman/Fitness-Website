55eddde101f855ea269de8db40303905929e06f9
marcojdelarosa <marcojdelarosa+1@gmail.com>
2025-04-08 16:13:07 -0700

    added schema from class diagram

--- backend/db/schema.js
+++ backend/db/schema.js
@@ -6,21 +6,31 @@ export const usersTable = sqliteTable('users', {
   password: text('password').notNull(),
   firstName: text('firstName'),
   lastName: text('lastName'),
+  friends: text('friends')
 });
 
 export const workoutTable = sqliteTable('workouts', {
   workoutID: integer('workoutID').primaryKey().autoIncrement(),
   userId: integer('userId').notNull().references(() => usersTable.id),
   workoutDate: text('workoutDate').notNull(),
+  workoutPlanned: integer('workoutPlanned').notNull,
 });
 
 export const exerciseTable = sqliteTable('exercises', {
   exerciseID: integer('exerciseID').primaryKey().autoIncrement(),
   workoutID: integer('workoutID').notNull().references(() => usersTable.id),
-  exerciseType: integer('exerciseType').notNull(), // this refers to the id in exercises.json
+  typeID: integer('typeID').notNull(), // this refers to the id in exercises.json
   difficulty: integer('difficulty').notNull(),
   reps: integer('reps'),
   weight: integer('weight'),
   duration: integer('duration'),
   notes: text('notes'),
 })
+
+export const goalTable = sqliteTable('goals', {
+  goalID: integer('goalID').primaryKey().autoIncrement(),
+  startDate: text('startDate').notNull(),
+  endDate: text('endDate').notNull(),
+  streak: integer('streak'),
+  numWorkouts: integer('numWorkouts'),
+});
